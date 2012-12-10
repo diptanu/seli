89082616d81083328257fef25e92544686a74b31
Diptanu Choudhury <admin.nitjece@gmail.com>
2012-12-10 22:16:10 +0000

    Made the port, url and repo path configurable

--- src/seli/seli.go
+++ src/seli/seli.go
@@ -7,15 +7,24 @@ import (
 )
 
 var config_file string
+var port string
+var base_url string 
+var repo_path string
 
 func init() {
 	flag.StringVar(&config_file, "config", "~/seli.conf", "Path to the Seli Configuration file")
+	flag.StringVar(&port, "port", ":80", "Port on which seli would run")
+	flag.StringVar(&base_url, "baseurl", "/Custom/", "Base URL of the repo")
+	flag.StringVar(&repo_path, "repopath", "/mnt/", "Path of the filesystem")
 }
 
 func main() {
 	log.Println("Starting Seli")
 	flag.Parse()
 	log.Println("Config file is %s", config_file)
-	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/Users/diptanuc/Src/"))))
-	http.ListenAndServe(":8080", nil)
+	http.Handle(base_url, http.StripPrefix(base_url, http.FileServer(http.Dir(repo_path))))
+	err := http.ListenAndServe(port, nil)
+        if err != nil {
+         log.Fatal(err)
+        }
 }
