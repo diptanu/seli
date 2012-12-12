35bf34d498292498ce6b0d1a8670997a6c194690
Diptanu Choudhury <admin.nitjece@gmail.com>
2012-12-12 09:09:23 +0000

    Added the capability to configure the repo location in the filesystem

--- src/seli/seli.go
+++ src/seli/seli.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"flag"
 	"log"
+	"io/ioutil"
 )
 
 var config_file string
@@ -18,13 +19,31 @@ func init() {
 	flag.StringVar(&repo_path, "repopath", "/mnt/", "Path of the filesystem")
 }
 
+func upload_handler(w http.ResponseWriter, req *http.Request){
+	file, handler , err := req.FormFile("filedata")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(handler.Filename, data, 0777) 
+        if err != nil { 
+                log.Println(err) 
+        } 
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(req.Method))
+}
+
 func main() {
 	log.Println("Starting Seli")
 	flag.Parse()
 	log.Println("Config file is %s", config_file)
 	http.Handle(base_url, http.StripPrefix(base_url, http.FileServer(http.Dir(repo_path))))
+	http.HandleFunc("/upload", upload_handler)
 	err := http.ListenAndServe(port, nil)
         if err != nil {
-         log.Fatal(err)
+		log.Fatal(err)
         }
 }
